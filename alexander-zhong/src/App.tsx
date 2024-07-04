07e32c1adb063e449d63ea32d2f16e4395960d6d
alexander-zhong <alexanderzousky@gmail.com>
2024-07-03 20:49:49 -0700

    Configured tailwind

--- alexander-zhong/src/App.tsx
+++ alexander-zhong/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <h1>Testing</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ alexander-zhong/src/components/Nav.tsx
@@ -0,0 +1,5 @@
+function Nav() {
+  return <h1 className="text-2xl font-bold mb-4">Testing</h1>;
+}
+
+export default Nav;
