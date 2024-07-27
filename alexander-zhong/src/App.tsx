bdcfd5d32a8bcd2498ffe9d8e8c7812a50e7a2ae
alexander-zhong <alexanderzousky@gmail.com>
2024-07-26 17:18:02 -0700

    reset to listing experiences for now

--- alexander-zhong/src/App.tsx
+++ alexander-zhong/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Nav from "./components/Nav";
 import About from "./sections/About";
 import Menu from "./components/Menu";
-import Portfolio from "./sections/Portfolio";
 import ListPortfolio from "./sections/ListPortfolio";
 
 function App() {
